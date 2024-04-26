91b38d4191e6e67ec333afa131f93615df8f0021
hargun3045 <hargun3045@gmail.com>
2024-04-26 19:29:21 +0530

    avoid headful

--- ninjemail/utils/webdriver_utils.py
+++ ninjemail/utils/webdriver_utils.py
@@ -67,7 +67,9 @@ def create_driver(browser, captcha_extension=False, proxy=None):
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-gpu')
         options.add_experimental_option('prefs', {'intl.accept_languages': 'en-us'})
-        # options.add_argument('--headless=new')
+        options.add_argument('--headless=new')
+        options.add_argument("--remote-debugging-port=9222")
+        options.add_argument("--disable-dev-shm-usage")
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         if proxy:
             options.add_argument(f'--proxy-server={proxy}')
